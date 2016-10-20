7593dc50b372b4609eded8ba82f1325fd2f14d58
willforcecomputers <willforcecomputers@gmail.com>
2016-10-19 19:27:25 -0700

    Finished getbits
    finished and tested getbits func. Commented. added to morebits turn in
    program.

--- getbits.c
+++ getbits.c
@@ -4,5 +4,41 @@
  *  Created on: Oct 18, 2016
  *      Author: William
  */
+#include <stdio.h>
 
+int getbits(unsigned int num, int lsb, int msb) {
 
+	unsigned int bitnum = 0;
+	unsigned int i = 0;
+
+	for (i = (1 << msb); i >= (1 << lsb); i = (i >> 1)) {
+
+		if((num & i) > 0) {
+
+			bitnum += i;
+		}
+	}
+
+	return (bitnum >> lsb);
+}
+
+int main() {
+
+	int x = 752;
+	int y = 3;
+	int z = 6;
+
+	int a = 4223;
+	int b = 6;
+	int c = 12;
+
+	int j = 90675;
+	int k = 1;
+	int l = 13;
+
+	printf("getbits(%d,%d,%d) = [%d] Expecting [14]\n", x, y, z, getbits(x, y, z));
+	printf("getbits(%d,%d,%d) = [%d] Expecting [65]\n", a, b, c, getbits(a, b, c));
+	printf("getbits(%d,%d,%d) = [%d] Expecting [4377]\n", j, k, l, getbits(j, k, l));
+
+	return 1;
+}
--- morebits.c
+++ morebits.c
@@ -5,4 +5,44 @@
  *      Author: William
  */
 
+#include <stdio.h>
 
+int getbits(unsigned int num, int lsb, int msb) { //return value of bits between the lsb and msb values.
+
+	unsigned int bitnum = 0; //accumulator for ON bits
+	unsigned int i = 0; //bit to be tested
+
+	for (i = (1 << msb); i >= (1 << lsb); i = (i >> 1)) { //for each bitwise value in the range of bits
+
+		if((num & i) > 0) { //if specified bit exists in given number, add to accumulator
+
+			bitnum += i;
+		}
+	}
+
+	return (bitnum >> lsb);
+}
+
+int main() {
+
+	//varibales for run 1
+	int x = 752;
+	int y = 3;
+	int z = 6;
+
+	//variable for run 2
+	int a = 4223;
+	int b = 6;
+	int c = 12;
+
+	//variables for run 3
+	int j = 90675;
+	int k = 1;
+	int l = 13;
+
+	printf("getbits(%x,%d,%d) = [%x] Expecting [e]\n", x, y, z, getbits(x, y, z));
+	printf("getbits(%x,%d,%d) = [%x] Expecting [41]\n", a, b, c, getbits(a, b, c));
+	printf("getbits(%x,%d,%d) = [%x] Expecting [1119]\n", j, k, l, getbits(j, k, l));
+
+	return 1;
+}
