d26063e7d158b30e5191a2dad998e6972b187688
willforcecomputers <willforcecomputers@gmail.com>
2016-10-20 15:36:01 -0700

    Finished rotateleft
    Finished second function, rotateleft. Added function to morebits, and
    commented.

--- morebits.c
+++ morebits.c
@@ -23,26 +23,74 @@ int getbits(unsigned int num, int lsb, int msb) { //return value of bits between
 	return (bitnum >> lsb);
 }
 
+int rotate_left(unsigned int num, int width, int howmany) { //rotate given amount of leading bits to end of an int
+
+	unsigned int rotanum = num; //rotated number variable
+	unsigned int accunum = 0; //accumulator for value to be rotated
+	unsigned int i = 0;
+
+	for (i = (width - 1); i >= (width - howmany); i -= 1) { //for loop iterating through bitwise positions within original number
+
+		unsigned int checknum = (1 << i); //number to check for and add/subtract if bit is ON
+		if((num & checknum) > 0) { //if bit is on add bitshifted number to accumulator and subtract from rotated number
+
+			accunum = (accunum << 1);
+			accunum += (checknum >> (i));
+			rotanum -= checknum;
+
+		}
+
+		else { //otherwise just shift the accumulator
+			accunum = (accunum << 1);
+		}
+	}
+
+	rotanum = rotanum << (howmany); //shift rotated number over to make room for accumulator
+	rotanum += accunum; //add accumulator to rotated number
+	return rotanum;
+}
+
 int main() {
 
-	//varibales for run 1
+	//varibales for getbits run 1
 	int x = 752;
 	int y = 3;
 	int z = 6;
 
-	//variable for run 2
+	//variable for getbits run 2
 	int a = 4223;
 	int b = 6;
 	int c = 12;
 
-	//variables for run 3
+	//variables for getbits run 3
 	int j = 90675;
 	int k = 1;
 	int l = 13;
 
+	//varibales for rotateleft run 1
+	int q = 752;
+	int w = 10;
+	int e = 4;
+
+	//variable for rotateleft run 2
+	int r = 4223;
+	int t = 13;
+	int g = 5;
+
+	//variables for rotateleft run 3
+	int s = 90675;
+	int d = 17;
+	int f = 13;
+
+	//runs for getbits
 	printf("getbits(%x,%d,%d) = [%x] Expecting [e]\n", x, y, z, getbits(x, y, z));
 	printf("getbits(%x,%d,%d) = [%x] Expecting [41]\n", a, b, c, getbits(a, b, c));
 	printf("getbits(%x,%d,%d) = [%x] Expecting [1119]\n", j, k, l, getbits(j, k, l));
+
+	//runs for rotateleft
+	printf("rotate_left(%x,%d,%d) = [%x] Expecting [30b]\n", q, w, e, rotate_left(q, w, e));
+	printf("rotate_left(%x,%d,%d) = [%x] Expecting [ff0]\n", r, t, g, rotate_left(r, t, g));
+	printf("rotate_left(%x,%d,%d) = [%x] Expecting [7623]\n", s, d, f, rotate_left(s, d, f));
 
 	return 1;
 }
--- rotate_left.c
+++ rotate_left.c
@@ -6,3 +6,52 @@
  */
 
 
+#include <stdio.h>
+
+int rotateleft(unsigned int num, int width, int howmany) {
+
+	unsigned int rotanum = num;
+	unsigned int accunum = 0;
+	unsigned int i = 0;
+
+	for (i = (width - 1); i >= (width - howmany); i -= 1) {
+
+		unsigned int checknum = (1 << i);
+		if((num & checknum) > 0) {
+
+			accunum = (accunum << 1);
+			accunum += (checknum >> (i));
+			rotanum -= checknum;
+
+		}
+
+		else {
+			accunum = (accunum << 1);
+		}
+	}
+
+	rotanum = rotanum << (howmany);
+	rotanum += accunum;
+	return rotanum;
+}
+
+int main() {
+
+	int x = 752;
+	int y = 10;
+	int z = 4;
+
+	int a = 154;
+	int b = 8;
+	int c = 3;
+
+	int j = 90675;
+	int k = 17;
+	int l = 13;
+
+	printf("rotateleft(%d,%d,%d) = [%d] Expecting [779]\n", x, y, z, rotateleft(x, y, z));
+	printf("rotateleft(%d,%d,%d) = [%d] Expecting [212]\n", a, b, c, rotateleft(a, b, c));
+	printf("rotateleft(%d,%d,%d) = [%d] Expecting [30243]\n", j, k, l, rotateleft(j, k, l));
+
+	return 1;
+}
